0773ccbc069f47d9d1b38bff8281b91d80e69af0
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-03-01 13:16:13 +0100

    Add: Core functionality

--- script/main.js
+++ script/main.js
@@ -1,3 +1,19 @@
-(function () {
-    console.log("Hello");
-})();
+const submenus = document.querySelectorAll('.submenu');
+const submenuCategoryItems = document.querySelectorAll('.submenu-category');
+
+submenuCategoryItems.forEach(item => {
+    item.addEventListener('mouseover', showSubmenu);
+    item.addEventListener('mouseleave', hideSubmenu);
+})
+
+function showSubmenu(e) {
+    let currentElement = e.currentTarget;
+    let childElement = currentElement.querySelector('ul');
+    childElement.style.display = 'initial';
+}
+
+function hideSubmenu(e) {
+    let currentElement = e.currentTarget;
+    let childElement = currentElement.querySelector('ul');
+    childElement.style.display = 'none';
+}
