b5d6a2e9d96a8dd2fec1f9abc1d6bc4e5a323257
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-03-22 20:12:10 +0100

    style: standardize nav items

--- script/main.js
+++ script/main.js
@@ -1,19 +1,2 @@
-// const submenus = document.querySelectorAll('.submenu');
-// const submenuCategoryItems = document.querySelectorAll('.submenu-category');
+const hamburger = document.getElementById("hamburger");
 
-// submenuCategoryItems.forEach(item => {
-//     item.addEventListener('mouseover', showSubmenu);
-//     item.addEventListener('mouseleave', hideSubmenu);
-// })
-
-// function showSubmenu(e) {
-//     let currentElement = e.currentTarget;
-//     let childElement = currentElement.querySelector('ul');
-//     childElement.style.display = 'initial';
-// }
-
-// function hideSubmenu(e) {
-//     let currentElement = e.currentTarget;
-//     let childElement = currentElement.querySelector('ul');
-//     childElement.style.display = 'none';
-// }
