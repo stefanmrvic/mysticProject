88b15f37c86af0d51b6eb51b535ae98b140c1bd5
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-03-23 01:33:59 +0100

    add: hamburger menu functionality

--- script/main.js
+++ script/main.js
@@ -1,2 +1,8 @@
 const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector(".nav");
 
+hamburger.addEventListener('click', toggleHamburgerMenu);
+
+function toggleHamburgerMenu() {
+    nav.classList.toggle("active");
+}
