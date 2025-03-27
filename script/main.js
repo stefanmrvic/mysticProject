71ea8c99c87e4f94df816a44eae6d8943f29c095
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-03-27 18:09:12 +0100

    Add: Mobile Nav Submenu expanding and collapsing functionality

--- script/main.js
+++ script/main.js
@@ -6,3 +6,17 @@ hamburger.addEventListener('click', toggleHamburgerMenu);
 function toggleHamburgerMenu() {
     nav.classList.toggle("active");
 }
+
+const submenuParentItems = document.querySelectorAll(".submenu-category");
+
+submenuParentItems.forEach(submenuParentItem => {
+    submenuParentItem.addEventListener("click", toggleSubmenu);
+})
+
+function toggleSubmenu(e) {
+    // Prevents bubbling for "Web Development" submenu under "Services" submenu, 
+    // and thus preventing instant collapsing of "Web Development" submenu
+    e.stopPropagation();
+
+    e.currentTarget.classList.toggle("active");
+}
